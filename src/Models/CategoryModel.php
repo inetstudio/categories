9a27cce6f35027021417a02f5f6baf83366f9961
Konstantin Bukin <bukin@inetstudio.ru>
2017-09-07 21:08:20 +0300

    Add content conversions

--- src/Models/CategoryModel.php
+++ src/Models/CategoryModel.php
@@ -177,11 +177,17 @@ class CategoryModel extends Model implements HasMediaConversions
             foreach (config('categories.images.conversions') as $collection => $image) {
                 foreach ($image as $crop) {
                     foreach ($crop as $conversion) {
-                        $this->addMediaConversion($conversion['name'])
-                            ->quality($quality)
-                            ->width($conversion['size']['width'])
-                            ->height($conversion['size']['height'])
-                            ->performOnCollections($collection);
+                        $imageConversion = $this->addMediaConversion($conversion['name'])->quality($quality);
+
+                        if (isset($conversion['size']['width'])) {
+                            $imageConversion->width($conversion['size']['width']);
+                        }
+
+                        if (isset($conversion['size']['height'])) {
+                            $imageConversion->height($conversion['size']['height']);
+                        }
+
+                        $imageConversion->performOnCollections($collection);
                     }
                 }
             }
